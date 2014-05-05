f9ca5f48efc88e2a6e5d8fcd851caf01b2ef481a
Hlorofos <alex@privhosting.com>
2014-05-05 16:20:21 +0300

    fix for bug when chosen variation is == 0

--- src/GoogleAnalytics/Experiment.php
+++ src/GoogleAnalytics/Experiment.php
@@ -375,7 +375,7 @@ class Experiment {
 	public function chooseVariation($utmx = null, $utmxx = null, $setCookies = true) {
 		$variation = $this->getChosenVariation($utmx);
 		
-		if(!$variation) {
+		if($variation===null) {
 			$variation = $this->chooseNewVariation();
 			$this->setChosenVariation($variation, $utmx, $utmxx, $setCookies);
 		}
